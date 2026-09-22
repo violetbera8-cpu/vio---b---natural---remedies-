0d7276d205e1deec4cadd358dd341fe28a031e28
violetbera8-cpu <violetbera8@gmail.com>
2026-09-22 14:16:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function toggleMenu(){document.getElementById('navLinks').classList.toggle('open')}document.querySelectorAll('#navLinks a').forEach(a=>a.addEventListener('click',()=>document.getElementById('navLinks').classList.remove('open')));
